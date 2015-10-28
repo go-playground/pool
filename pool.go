04182abb9fee67f171fddf57010bcc88aac15e13
joeybloggs <Dean.Karn@metricaid.com>
2015-10-28 14:29:44 -0400

    Initial commit, will rearrange
    * will rearrange code to not start consuming until all have been added
    to the Queue otherwise these's too much contention when cancelling etc.

---
+++ pool.go
@@ -0,0 +1,141 @@
+package pool
+
+import (
+	"fmt"
+	"log"
+	"reflect"
+	"sync"
+)
+
+// Pool Contains channels to instantiate pool
+type Pool struct {
+	jobs          chan *Job
+	results       chan interface{}
+	cancel        chan struct{}
+	wg            *sync.WaitGroup
+	jobsClosed    bool
+	resultsClosed bool
+	jobsLock      sync.RWMutex
+	resultsLock   sync.RWMutex
+}
+
+// JobFunc ...
+type JobFunc func(job *Job)
+
+// ... make this into sync.Pool
+type Job struct {
+	fn     JobFunc
+	params []interface{}
+	pool   *Pool
+}
+
+func (j *Job) Params() []interface{} {
+	return j.params
+}
+
+func (j *Job) Cancel() {
+	j.pool.Cancel()
+}
+
+func (j *Job) Return(result interface{}) {
+	j.pool.results <- result
+}
+
+// NewPool initializes pool
+func NewPool(consumers int, jobs int) *Pool {
+
+	// make this a sync pool as well
+	p := &Pool{
+		wg:            new(sync.WaitGroup),
+		jobs:          make(chan *Job, jobs),
+		results:       make(chan interface{}, jobs),
+		cancel:        make(chan struct{}),
+		jobsClosed:    false,
+		resultsClosed: false,
+	}
+
+	for i := 0; i < consumers; i++ {
+		go func(p *Pool) {
+			for {
+				select {
+				case j := <-p.jobs:
+					if reflect.ValueOf(j).IsNil() {
+						return
+					}
+					defer p.wg.Done()
+					log.Println("Running")
+					j.fn(j)
+					// j.fn(p.results, p.cancel, c.param...)
+				case <-p.cancel:
+					fmt.Println("Cancelling")
+					p.cancelJobs()
+					return
+				}
+			}
+		}(p)
+	}
+	return p
+}
+
+func (p *Pool) cancelJobs() {
+	for range p.jobs {
+		p.wg.Done()
+	}
+
+	p.closeJobsChannel()
+	p.closeResultsChannel()
+}
+
+func (p *Pool) closeJobsChannel() {
+	p.jobsLock.Lock()
+	defer p.jobsLock.Unlock()
+	if !p.jobsClosed {
+		p.jobsClosed = true
+		close(p.jobs)
+	}
+}
+
+func (p *Pool) closeResultsChannel() {
+	p.resultsLock.Lock()
+	defer p.resultsLock.Unlock()
+	if !p.resultsClosed {
+		p.resultsClosed = true
+		close(p.results)
+	}
+}
+
+// Queue ...
+func (p *Pool) Queue(fn JobFunc, params ...interface{}) {
+
+	job := &Job{
+		fn:     fn,
+		params: params,
+		pool:   p,
+	}
+
+	p.wg.Add(1)
+	p.jobs <- job
+}
+
+// Cancel cancels all jobs not already running
+func (p *Pool) Cancel() {
+	p.cancel <- struct{}{}
+	p.cancelJobs()
+}
+
+// Results ...
+func (p *Pool) Results() <-chan interface{} {
+
+	p.closeJobsChannel()
+
+	go func() {
+		log.Println("Waiting for completion")
+		p.wg.Wait()
+		fmt.Println("Closing WAIT FUNCTION!")
+		p.closeResultsChannel()
+	}()
+
+	log.Println("Returning Results Channel")
+
+	return p.results
+}
---
+++ pool_test.go
@@ -0,0 +1,63 @@
+package pool
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"testing"
+	"time"
+
+	. "gopkg.in/go-playground/assert.v1"
+)
+
+// NOTES:
+// - Run "go test" to run tests
+// - Run "gocov test | gocov report" to report on test converage by file
+// - Run "gocov test | gocov annotate -" to report on all code and functions, those ,marked with "MISS" were never called
+//
+// or
+//
+// -- may be a good idea to change to output path to somewherelike /tmp
+// go test -coverprofile cover.out && go tool cover -html=cover.out -o cover.html
+//
+
+func TestMain(m *testing.M) {
+
+	// setup
+
+	os.Exit(m.Run())
+
+	// teardown
+}
+
+func TestGoPool(t *testing.T) {
+
+	pool := NewPool(3, 15)
+
+	fn := func(job *Job) {
+
+		i := job.Params()[0].(int)
+		if i == 3 {
+			fmt.Println("function calling cancel")
+			job.Cancel()
+			return
+		}
+		log.Println("Executing Function: ", i)
+		time.Sleep(time.Second * 1)
+		job.Return(i)
+	}
+
+	for i := 0; i < 9; i++ {
+		pool.Queue(fn, i)
+	}
+
+	var count int
+
+	for r := range pool.Results() {
+		count++
+		log.Println("Result :", r)
+	}
+
+	// Equal(t, count, 9)
+	Equal(t, true, true)
+}
